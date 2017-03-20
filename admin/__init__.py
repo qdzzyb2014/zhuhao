04ec2311c3f6d3e5f6b4cb7b404cdd42bc874c5c
qdzzyb2014 <31366556@qq.com>
2017-03-20 20:36:42 +0800

    add admin

--- admin/__init__.py
+++ admin/__init__.py
@@ -1,9 +1,15 @@
 from flask_admin import Admin
-from flask_admin import AdminIndexView
+from fo.models import Registration
+from .views import UserRegisterView
+from fo.extensions import db
 
 
 def create_admin(app):
     admin = Admin(
         app, url='/admin', template_mode='bootstrap3'
     )
+
+    admin.add_view(UserRegisterView(
+        Registration, db.session, '报名用户', endpoint='register'))
+
     return admin
---
+++ admin/views.py
@@ -0,0 +1,23 @@
+from flask_admin.contrib.sqla import ModelView
+
+
+class UserRegisterView(ModelView):
+    can_delete = True
+    can_edit = True
+    can_export = True
+
+    column_list = [
+        'name',
+        'company',
+        'job_title',
+        'contact_way',
+        'main_industry'
+    ]
+
+    column_labels = {
+        'name': '姓名',
+        'company': '公司名称',
+        'job_title': '职称',
+        'contact_way': '联系方式',
+        'main_industry': '主营产业'
+    }
